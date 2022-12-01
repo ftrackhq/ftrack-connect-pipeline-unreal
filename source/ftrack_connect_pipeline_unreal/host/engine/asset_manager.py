21052bb51d68841ef0f4946a649850e6594b2f0d
Lluis Ftrack <lluis.casals@ftrack.com>
2022-12-01 09:53:17 +0100

    AM engine implemented

--- source/ftrack_connect_pipeline_unreal/host/engine/asset_manager.py
+++ source/ftrack_connect_pipeline_unreal/host/engine/asset_manager.py
@@ -120,83 +120,9 @@ class UnrealAssetManagerEngine(AssetManagerEngine):
         select the given *asset_info*.
         Returns status and result
         '''
-        start_time = time.time()
-        status = core_constants.UNKNOWN_STATUS
-        result = []
-        message = None
-
-        plugin_type = core_constants.PLUGIN_AM_ACTION_TYPE
-        plugin_name = None
-        if plugin:
-            plugin_type = '{}.{}'.format('asset_manager', plugin['type'])
-            plugin_name = plugin.get('name')
-
-        result_data = {
-            'plugin_name': plugin_name,
-            'plugin_type': plugin_type,
-            'method': 'select_asset',
-            'status': status,
-            'result': result,
-            'execution_time': 0,
-            'message': message,
-        }
-
-        self.asset_info = asset_info
-        dcc_object = self.DccObject(
-            from_id=asset_info[asset_const.ASSET_INFO_ID]
+        return super(UnrealAssetManagerEngine, self).select_asset(
+            asset_info=asset_info, options=options, plugin=plugin
         )
-        self.dcc_object = dcc_object
-
-        if options.get('clear_selection'):
-            unreal_utils.deselect_all()
-
-        nodes = (
-            unreal_utils.get_connected_objects_from_dcc_object(
-                self.dcc_object.name
-            )
-            or []
-        )
-
-        # Filter out the dcc object
-        nodes = list(filter(lambda x: x.name != self.dcc_object.name, nodes))
-
-        for node in nodes:
-            try:
-                unreal_utils.add_node_to_selection(node)
-                result.append(str(node))
-                status = core_constants.SUCCESS_STATUS
-            except Exception as error:
-                message = str(
-                    'Could not select the node {}, error: {}'.format(
-                        str(node), error
-                    )
-                )
-                self.logger.error(message)
-                status = core_constants.ERROR_STATUS
-
-            bool_status = core_constants.status_bool_mapping[status]
-            if not bool_status:
-                end_time = time.time()
-                total_time = end_time - start_time
-
-                result_data['status'] = status
-                result_data['result'] = result
-                result_data['execution_time'] = total_time
-                result_data['message'] = result['message'] = message
-
-                self._notify_client(plugin, result_data)
-                return status, result
-
-        end_time = time.time()
-        total_time = end_time - start_time
-
-        result_data['status'] = status
-        result_data['result'] = result
-        result_data['execution_time'] = total_time
-
-        self._notify_client(plugin, result_data)
-
-        return status, result
 
     @unreal_utils.run_in_main_thread
     def select_assets(self, assets, options=None, plugin=None):
--- source/ftrack_connect_pipeline_unreal/utils/custom_commands.py
+++ source/ftrack_connect_pipeline_unreal/utils/custom_commands.py
@@ -6,6 +6,7 @@ import os
 import logging
 import sys
 import subprocess
+import json
 
 import unreal
 
@@ -119,25 +120,26 @@ def delete_node(node_name):
     unreal.EditorAssetLibrary.delete_asset(node_name)
     pass
 
-# TODO: need this one for unreal
-# (Only DCC with no live connections)
-# def get_connected_objects_from_dcc_object(dcc_object_name):
-#     '''Return all objects connected to the given *dcc_object_name*'''
-#     # Get Unique id for a node using rt.getHandleByAnim(obj) and get the node
-#     # from the unique id using rt.getAnimByHandler(id) please see the following
-#     # link for more info: https://help.autodesk.com/view/MAXDEV/2023/ENU/?guid=GUID-25211F97-E81A-4D49-AFB6-50B30894FBEB
-#     objects = []
-#     dcc_object_node = rt.getNodeByName(dcc_object_name, exact=True)
-#     if not dcc_object_node:
-#         return
-#     id_value = rt.getProperty(dcc_object_node, asset_const.ASSET_INFO_ID)
-#     for parent in rt.rootScene.world.children:
-#         children = [parent] + collect_children_nodes(parent)
-#         for obj in children:
-#             if rt.isProperty(obj, "ftrack"):
-#                 if id_value == rt.getProperty(obj, "ftrack"):
-#                     objects.append(obj)
-#     return objects
+
+def get_connected_objects_from_dcc_object(dcc_object_name):
+    '''Return all objects connected to the given *dcc_object_name*'''
+    objects = []
+    dcc_object_node = None
+    ftrack_nodes = get_ftrack_nodes()
+    for node in ftrack_nodes:
+        if node.endswith("{}.json".format(dcc_object_name))
+            dcc_object_node = node
+            break
+    if not dcc_object_node:
+        return
+    with open(dcc_object_node, 'r') as openfile:
+        param_dict = json.load(openfile)
+    id_value = param_dict.get(asset_const.ASSET_INFO_ID)
+    for obj in get_current_scene_objects():
+        ftrack_value = unreal.EditorAssetLibrary.get_metadata_tag(obj, "ftrack")
+        if id_value == ftrack_value:
+            objects.append(obj)
+    return objects
 
 
 def get_all_sequences(as_names=True):
@@ -164,14 +166,12 @@ def select_all():
 
 def deselect_all():
     '''Clear the selection'''
-    # TODO: need this
     # rt.clearSelection()
     pass
 
 
 def add_node_to_selection(node):
     '''Add the given *node* to the current selection'''
-    #TODO: need this one
     # rt.selectMore(node)
     pass
 
@@ -207,11 +207,9 @@ def open_file(path, options=None):
     pass
 
 
-def import_file(path, options=None):
-    '''Native import file function'''
-    # TODO: import the file to the project
-    # return cmds.file(path, o=True, f=True)
-    pass
+def import_file(asset_import_task):
+    '''Native import file function using the object unreal.AssetImportTask() given as *asset_import_task*'''
+    return unreal.AssetToolsHelpers.get_asset_tools().import_asset_tasks([asset_import_task])
 
 
 def save_file(save_path, context_id=None, session=None, temp=True, save=True):
