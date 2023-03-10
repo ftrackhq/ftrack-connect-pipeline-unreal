46d23f070375c990b5aefd83974486786b346261
Lluis Ftrack <lluis.casals@ftrack.com>
2023-03-10 09:51:46 +0100

    suport only image sequences

--- resource/plugins/python/publisher/collectors/unreal_sequence_publisher_collector.py
+++ resource/plugins/python/publisher/collectors/unreal_sequence_publisher_collector.py
@@ -5,7 +5,6 @@ import unreal
 import ftrack_api
 
 from ftrack_connect_pipeline_unreal import plugin
-from ftrack_connect_pipeline_unreal import utils as unreal_utils
 
 
 class UnrealSequencePublisherCollectorPlugin(
@@ -15,62 +14,13 @@ class UnrealSequencePublisherCollectorPlugin(
 
     plugin_name = 'unreal_sequence_publisher_collector'
 
-    def select(self, context_data=None, data=None, options=None):
-        '''Select all the sequences in the given plugin *options*'''
-        selected_items = options.get('selected_items', [])
-        return selected_items
-
-    def fetch(self, context_data=None, data=None, options=None):
-        '''Fetch all level sequences from the level/map'''
-        result = []
-        collected_objects = unreal_utils.get_all_sequences()
-
-        # Find the selected sequence in level edit or in content browser
-        sequence_name_sel = None
-        for actor in unreal.EditorLevelLibrary.get_selected_level_actors():
-            if (
-                actor.static_class()
-                == unreal.LevelSequenceActor.static_class()
-            ):
-                sequence_name_sel = actor.get_name()
-                break
-        if not sequence_name_sel:
-            for asset in unreal.EditorUtilityLibrary.get_selected_assets():
-                if asset.static_class() == unreal.LevelSequence.static_class():
-                    sequence_name_sel = asset.get_name()
-                    break
-
-        for sequence_name in collected_objects:
-            fetch_data = {'value': sequence_name}
-            if sequence_name == sequence_name_sel:
-                fetch_data['default'] = True
-            result.append(fetch_data)
-
-        return result
-
     def run(self, context_data=None, data=None, options=None):
-        '''Return the name of file path or sequence from plugin *options*'''
+        '''Return the name of file path from plugin *options*'''
 
-        mode = options.get('mode', 'pickup')
-        if mode == 'pickup':
-            file_path = options.get('media_path')
-            if not file_path:
-                return False, {'message': 'No render media file path chosen.'}
-            return [{'media_path': file_path}]
-        elif mode == 'render':
-            level_sequence_name = options.get('level_sequence_name')
-            if not level_sequence_name:
-                return False, {'message': 'No level sequence chosen.'}
-            return [
-                {
-                    'level_sequence_name': level_sequence_name,
-                    'render_path': options.get('render_path'),
-                }
-            ]
-        else:
-            return False, {
-                'message': 'Unsupported collect mode: {0}'.format(mode)
-            }
+        file_path = options.get('media_path')
+        if not file_path:
+            return False, {'message': 'No render media file path chosen.'}
+        return [{'media_path': file_path}]
 
 
 def register(api_object, **kw):
--- resource/plugins/python/publisher/collectors/widget/unreal_sequence_publisher_collector_options.py
+++ resource/plugins/python/publisher/collectors/widget/unreal_sequence_publisher_collector_options.py
@@ -1,7 +1,6 @@
 # :coding: utf-8
 # :copyright: Copyright (c) 2014-2023 ftrack
 import os
-from functools import partial
 
 import unreal
 
@@ -19,12 +18,7 @@ class UnrealSequencePublisherCollectorOptionsWidget(BaseOptionsWidget):
     '''Unreal sequence collector widget plugin'''
 
     # Run fetch function on widget initialization
-    auto_fetch_on_init = True
-
-    @property
-    def image_sequence(self):
-        '''Return True if the collector should collect image sequences, False if it should collect reviewable/movie.'''
-        return self.options.get('image_sequence', True) is True
+    auto_fetch_on_init = False
 
     @property
     def media_path(self):
@@ -42,47 +36,14 @@ class UnrealSequencePublisherCollectorOptionsWidget(BaseOptionsWidget):
         if media_path is not None and len(media_path) > 0:
             self.set_option_result(media_path, 'media_path')
             # Remember last used path
-            key = '{}_media_path'.format(
-                'image_sequence' if self.image_sequence else 'movie'
-            )
-            unreal_utils.update_project_settings({key: media_path})
+            unreal_utils.update_project_settings({'media_path': media_path})
         else:
-            media_path = '<please choose a {}>'.format(
-                'image sequence' if self.image_sequence else 'movie'
-            )
+            media_path = '<please choose am image sequence>'
             self.set_option_result(None, 'media_path')
 
         # Update UI
         self._media_path_le.setText(media_path)
         self._media_path_le.setToolTip(media_path)
-
-    @property
-    def render_path(self):
-        '''Return the render path from options'''
-        result = self.options.get('render_path')
-        if result:
-            result = result.strip()
-            if len(result) == 0:
-                result = None
-        return result
-
-    @render_path.setter
-    def render_path(self, render_path):
-        '''Store *render_path* in options and update widgets'''
-        if render_path is not None and len(render_path) > 0:
-            self.set_option_result(render_path, 'render_path')
-            # Remember last used path
-            key = '{}_render_path'.format(
-                'image_sequence' if self.image_sequence else 'movie'
-            )
-            unreal_utils.update_project_settings({key: render_path})
-        else:
-            render_path = '<please choose>'
-            self.set_option_result(None, 'render_path')
-
-        # Update UI
-        self._render_path_le.setText(render_path)
-        self._render_path_le.setToolTip(render_path)
 
     def __init__(
         self,
@@ -107,34 +68,13 @@ class UnrealSequencePublisherCollectorOptionsWidget(BaseOptionsWidget):
             asset_type_name=asset_type_name,
         )
 
-    def add_sequences(self):
-        '''Add Unreal level sequence names to the widget.'''
-        self._sequences_cb.clear()
-        if not self.unreal_sequences:
-            self._sequences_cb.setDisabled(True)
-            self._sequences_cb.addItem(
-                'No suitable level sequences found, please add to level.'
-            )
-        else:
-            self._sequences_cb.setDisabled(False)
-            for index, data in enumerate(self.unreal_sequences):
-                self._sequences_cb.addItem(data['value'])
-                if data.get('default') is True:
-                    self._sequences_cb.setCurrentIndex(index)
-
     def build(self):
         '''Build the options widget'''
         super(UnrealSequencePublisherCollectorOptionsWidget, self).build()
 
-        bg = QtWidgets.QButtonGroup(self)
-
-        self._pickup_rb = QtWidgets.QRadioButton(
-            'Pick up rendered {}:'.format(
-                'image sequence' if self.image_sequence else 'movie'
-            )
+        self._pickup_label = QtWidgets.QLabel(
+            'Pick up rendered image sequence: '
         )
-        bg.addButton(self._pickup_rb)
-        self.layout().addWidget(self._pickup_rb)
 
         self._browse_media_path_widget = QtWidgets.QWidget()
         self._browse_media_path_widget.setLayout(QtWidgets.QHBoxLayout())
@@ -158,129 +98,22 @@ class UnrealSequencePublisherCollectorOptionsWidget(BaseOptionsWidget):
 
         # Use previous value if available
         path = self.media_path
-        if path is None or len(path) == 0:
-            key = '{}_media_path'.format(
-                'image_sequence' if self.image_sequence else 'movie'
-            )
-            path = unreal_utils.get_project_settings().get(key)
+        if not path or len(path) == 0:
+            path = unreal_utils.get_project_settings().get('media_path')
         self.media_path = path
 
-        self._render_rb = QtWidgets.QRadioButton('Render from level sequence:')
-        bg.addButton(self._render_rb)
-        self.layout().addWidget(self._render_rb)
-
-        self._render_widget = QtWidgets.QWidget()
-        self._render_widget.setLayout(QtWidgets.QVBoxLayout())
-        self._render_widget.layout().setContentsMargins(0, 0, 0, 0)
-        self._render_widget.layout().setSpacing(0)
-
-        self._sequences_cb = QtWidgets.QComboBox()
-        self._sequences_cb.setToolTip(self.description)
-        self._render_widget.layout().addWidget(self._sequences_cb)
-
-        self._browse_render_path_widget = QtWidgets.QWidget()
-        self._browse_render_path_widget.setLayout(QtWidgets.QHBoxLayout())
-        self._browse_render_path_widget.layout().setContentsMargins(0, 0, 0, 0)
-        self._browse_render_path_widget.layout().setSpacing(0)
-
-        self._browse_render_path_widget.layout().addWidget(
-            QtWidgets.QLabel('Render folder:')
-        )
-
-        self._render_path_le = QtWidgets.QLineEdit()
-        self._render_path_le.setReadOnly(True)
-
-        self._browse_render_path_widget.layout().addWidget(
-            self._render_path_le, 20
-        )
-
-        self._browse_render_path_btn = QtWidgets.QPushButton('BROWSE')
-        self._browse_render_path_btn.setObjectName('borderless')
-
-        self._browse_render_path_widget.layout().addWidget(
-            self._browse_render_path_btn
-        )
-        self._render_widget.layout().addWidget(self._browse_render_path_widget)
-
-        # Use previous value if available
-        path = self.render_path
-        if path is None or len(path) == 0:
-            key = '{}_render_path'.format(
-                'image_sequence' if self.image_sequence else 'movie'
-            )
-            path = unreal_utils.get_project_settings().get(key)
-        self.render_path = path
-
-        self.layout().addWidget(self._render_widget)
-
-        if self.options.get('level_sequence_name'):
-            self.unreal_sequences.append(
-                {'value': self.options.get('level_sequence_name')}
-            )
-
-        self.add_sequences()
-
-        if 'mode' not in self.options:
-            self.set_option_result('pickup', 'mode')  # Set default mode
-        mode = self.options['mode'].lower()
-        if mode == 'render':
-            self._render_rb.setChecked(True)
-        elif mode == 'pickup':
-            self._pickup_rb.setChecked(True)
-        else:
-            # Fallback to pickup mode
-            self._pickup_rb.setChecked(True)
         self.report_input()
 
     def post_build(self):
         super(UnrealSequencePublisherCollectorOptionsWidget, self).post_build()
 
         self._browse_media_path_btn.clicked.connect(
-            self._show_media_path_dialog
+            self._show_image_sequence_dialog
         )
-
-        self._browse_render_path_btn.clicked.connect(
-            self._show_render_path_dialog
-        )
-        update_fn = partial(self.set_option_result, key='level_sequence_name')
-        self._sequences_cb.currentTextChanged.connect(update_fn)
-        if self.unreal_sequences:
-            self.set_option_result(
-                self._sequences_cb.currentText(), key='level_sequence_name'
-            )
-
-        self._render_rb.clicked.connect(self._update_render_mode)
-        self._pickup_rb.clicked.connect(self._update_render_mode)
-        self._update_render_mode()
-
-    def _update_render_mode(self):
-        '''Update widget based on selected render mode'''
-        mode = 'pickup'
-        if self._render_rb.isChecked():
-            mode = 'render'
-        self.set_option_result(mode, 'mode')
-
-        self._browse_media_path_widget.setVisible(mode == 'pickup')
-        self._render_widget.setVisible(mode == 'render')
-
-        self.report_input()
-
-    def on_fetch_callback(self, result):
-        '''This function is called by the _set_internal_run_result function of
-        the BaseOptionsWidget'''
-        self.unreal_sequences = result
-        self.add_sequences()
-
-    def _show_media_path_dialog(self):
-        '''Shows the file dialog for choosing media path'''
-        if self.image_sequence:
-            self._show_image_sequence_dialog()
-        else:
-            self._show_movie_dialog()
 
     def _show_image_sequence_dialog(self):
         '''Shows the file dialog for image sequences'''
-        if self.media_path is None:
+        if not self.media_path:
             start_dir = os.path.realpath(
                 os.path.join(
                     unreal.SystemLibrary.get_project_saved_directory(),
@@ -290,10 +123,12 @@ class UnrealSequencePublisherCollectorOptionsWidget(BaseOptionsWidget):
         else:
             start_dir = os.path.dirname(self._media_path_le.text())
 
+        # TODO: add all supported file formats
+        # supported_file_formats = [".bmp","float",".pcx",".png",".psd",".tga",".jpg",".exr",".dds", ".hdr"]
         media_path = QtWidgets.QFileDialog.getExistingDirectory(
             caption='Choose directory containing rendered image sequence',
             dir=start_dir,
-            options=QtWidgets.QFileDialog.ShowDirsOnly,
+            options=QtWidgets.QFileDialog.setNameFilter(("Images (*.png *.xpm *.jpg)"))
         )
 
         if not media_path:
@@ -316,81 +151,21 @@ class UnrealSequencePublisherCollectorOptionsWidget(BaseOptionsWidget):
 
         self.report_input()
 
-    def _show_movie_dialog(self):
-        '''Shows the file dialog to select a movie'''
-
-        if self.media_path is None:
-            start_dir = os.path.realpath(
-                os.path.join(
-                    unreal.SystemLibrary.get_project_saved_directory(),
-                    "VideoCaptures",
-                )
-            )
-        else:
-            start_dir = os.path.dirname(self._media_path_le.text())
-
-        (
-            movie_path,
-            unused_selected_filter,
-        ) = QtWidgets.QFileDialog.getOpenFileName(
-            caption='Choose rendered movie file',
-            dir=start_dir,
-            filter='Avi files (*.avi);;Movies (*.mov);;All files (*)',
-        )
-
-        if not movie_path:
-            return
-
-        self.media_path = os.path.normpath(movie_path)
-
-        self.report_input()
-
-    def _show_render_path_dialog(self):
-        '''Show the file dialog to select a render path'''
-        if self.render_path is None:
-            start_dir = os.path.realpath(
-                os.path.join(
-                    unreal.SystemLibrary.get_project_saved_directory(),
-                    "VideoCaptures",
-                )
-            )
-        else:
-            start_dir = os.path.dirname(self._render_path_le.text())
-
-        render_path = QtWidgets.QFileDialog.getExistingDirectory(
-            caption='Choose render output directory',
-            dir=start_dir,
-            options=QtWidgets.QFileDialog.ShowDirsOnly,
-        )
-
-        if not render_path:
-            return
-
-        self.render_path = os.path.normpath(render_path)
-
     def report_input(self):
         '''(Override) Amount of collected objects has changed, notify parent(s)'''
         status = False
-        if self._pickup_rb.isChecked():
-            num_objects = (
-                1
-                if self.media_path is not None and len(self.media_path) > 0
-                else 0
+        num_objects = (
+            1
+            if self.media_path and len(self.media_path) > 0
+            else 0
+        )
+        if num_objects > 0:
+            message = '1 {} selected'.format(
+                'image sequence' if self.image_sequence else 'movie'
             )
-            if num_objects > 0:
-                message = '1 {} selected'.format(
-                    'image sequence' if self.image_sequence else 'movie'
-                )
-                status = True
-            else:
-                message = 'No media selected!'
+            status = True
         else:
-            num_objects = 1 if self._sequences_cb.isEnabled() else 0
-            if num_objects > 0:
-                message = '1 level sequence selected'
-                status = True
-            else:
-                message = 'No level sequence selected!'
+            message = 'No media selected!'
         self.inputChanged.emit(
             {
                 'status': status,
